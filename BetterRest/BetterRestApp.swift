39fe5a5b37a3d4f01fae0368d3e000bf207e08c7
walker-sean <walker.se@northeastern.edu>
2023-08-13 19:01:36 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sean Walker on 8/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

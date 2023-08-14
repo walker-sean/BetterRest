43dc431baeb091e74bc6cf50c161c28e18d98bb7
walker-sean <walker.se@northeastern.edu>
2023-08-14 18:32:15 -0400

    Wow dates are sick

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,7 +12,16 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please select a date: ", selection: $wakeUp, in: ...Date.now)
+        VStack {
+            DatePicker("Please select a date: ", selection: $wakeUp, in: ...Date.now)
+            Text(wakeUp.formatted(date: .long , time: .shortened))
+        }
+    }
+    
+    func trivalExample() {
+        var components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minutes = components.minute ?? 0
     }
 }
 
